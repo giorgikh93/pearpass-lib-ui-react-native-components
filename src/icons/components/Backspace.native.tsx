86051b02beca1833456a5b0d809b51f768db5292
ndrkltsk <aklitsuk@gmail.com>
2026-04-09 10:31:10 +0200

    feat: add new icons and update existing ones for improved functionality (#70)
    * feat: add new icons and update existing ones for improved functionality
    
    * feat: add Backspace icon to icon components

--- src/icons/components/Backspace.native.tsx
+++ src/icons/components/Backspace.native.tsx
@@ -2,12 +2,12 @@
 import * as React from "react";
 import Svg, { Path } from "react-native-svg";
 import type { SvgProps } from "react-native-svg";
-const SvgLaptopWindows = (props: SvgProps) => (
+const SvgBackspace = (props: SvgProps) => (
   <Svg width={24} height={24} fill="none" viewBox="0 0 24 24" {...props}>
     <Path
       fill="currentColor"
-      d="M20 18.5v-1c1.1 0 1.99-.9 1.99-2l.01-10c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2v1H0v2h24v-2zM4 5.5h16v10H4z"
+      d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2m-3 12.59L17.59 17 14 13.41 10.41 17 9 15.59 12.59 12 9 8.41 10.41 7 14 10.59 17.59 7 19 8.41 15.41 12z"
     />
   </Svg>
 );
-export default SvgLaptopWindows;
+export default SvgBackspace;
